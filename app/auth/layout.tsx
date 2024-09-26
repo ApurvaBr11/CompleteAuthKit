1dcd7041f7140691e679727e8da89baa5b252cf0
apurvaBr11 <apurvkashyappurnea@gmail.com>
2024-09-26 11:11:04 +0530

    uifix2

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Authlayout = ({children}:{children:React.ReactNode}) => {
   return (
-    <div className='h-full flex justify-center items-center bg-yellow-100'>{children}</div>
+    <div className='h-full flex justify-center items-center bg-gradient-to-br from-gray-50 to-gray-100'>{children}</div>
   )
 }
 
